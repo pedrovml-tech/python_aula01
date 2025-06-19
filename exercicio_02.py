4a48f4a9215341041ec54ee9fdcf8b66397d8c28
pedro.vml <pedro.vml@proton.me>
2025-06-18 22:30:36 -0300

    exercicio 2

---
+++ exercicio_02.py
@@ -0,0 +1,6 @@
+# Crie um programa para retornar a soma de 2 valores digitados pelo usuário
+
+valor_1 = int(input("Digite um valor: "))
+valor_2 = int(input("Digite outro valor: "))
+
+print(valor_1 + valor_2)
